e93f05b8d0f523b408db78414097059a9b71569a
ardianilyas <ardianilyas@gmail.com>
2025-11-23 22:40:21 +0700

    feat: update env config add NODE_ENV

--- src/config/env.ts
+++ src/config/env.ts
@@ -5,6 +5,7 @@ dotenv.config();
 export const env = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || "development",
+    LOG_LEVEL: process.env.LOG_LEVEL || "info",
     DATABASE_URL: process.env.DATABASE_URL,
     JWT_SECRET: process.env.JWT_SECRET
 }
