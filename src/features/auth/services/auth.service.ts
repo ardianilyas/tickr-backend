9d3597fa8b5a33fb166356f6a13ff52f96090e14
ardianilyas <ardianilyas@gmail.com>
2025-12-01 22:08:33 +0700

    feat: update throw error code

--- src/features/auth/services/auth.service.ts
+++ src/features/auth/services/auth.service.ts
@@ -1,3 +1,5 @@
+import { ConflictError } from "../../../errors/ConflictError";
+import { UnauthorizedError } from "../../../errors/UnauthorizedError";
 import { comparePassword, hashPassword } from "../../../utils/bcrypt";
 import { signAccessToken } from "../../../utils/jwt";
 import { AuthRepository } from "../repositories/auth.repository";
@@ -7,7 +9,7 @@ export class AuthService {
     static async register(data: RegisterSchema) {
         const exists = await AuthRepository.getUserByEmail(data.email);
 
-        if (exists) throw new Error("User already registered");
+        if (exists) throw new ConflictError("User already registered");
 
         const hashed = await hashPassword(data.password);
 
@@ -17,11 +19,11 @@ export class AuthService {
     static async login(data: LoginSchema) {
         const user = await AuthRepository.getUserByEmail(data.email);
 
-        if (!user) throw new Error("Invalid credentials");
+        if (!user) throw new UnauthorizedError("Invalid credentials");
 
         const validPassword = await comparePassword(data.password, user.password);
 
-        if (!validPassword) throw new Error("Invalid credentials");
+        if (!validPassword) throw new UnauthorizedError("Invalid credentials");
 
         const token = signAccessToken({ id: user.id, role: user.role, name: user.name });
 
