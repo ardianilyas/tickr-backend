c6f32f9fa7448c926dd476473a168ed5a8f19d90
ardianilyas <ardianilyas@gmail.com>
2025-12-06 15:44:02 +0700

    feat: update env config

--- src/config/env.ts
+++ src/config/env.ts
@@ -27,4 +27,8 @@ export const env = {
   DATABASE_URL: getEnv("DATABASE_URL"),
   JWT_SECRET: getEnv("JWT_SECRET"),
   RESEND_API_KEY: getEnv("RESEND_API_KEY"),
+
+  REDIS_PORT: getEnv("REDIS_PORT", 6379),
+  REDIS_HOST: getEnv("REDIS_HOST", "localhost"),
+  REDIS_PASSWORD: getEnv("REDIS_PASSWORD", ""),
 };
