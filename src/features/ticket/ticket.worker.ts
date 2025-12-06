29c7f6b6abfe525645e2df76d623083000603440
ardianilyas <ardianilyas@gmail.com>
2025-12-06 22:05:54 +0700

    feat: update ticket worker change console.log to local logger

--- src/features/ticket/ticket.worker.ts
+++ src/features/ticket/ticket.worker.ts
@@ -1,4 +1,6 @@
 import { TicketPriority } from "../../generated/prisma/enums";
+import { resend } from "../../lib/resend";
+import { logger } from "../../utils/logger";
 import { TICKET_QUEUE_NAME } from "./ticket.queue";
 import { TicketJobData } from "./ticket.types";
 
@@ -8,11 +10,22 @@ export const ticketWorker = {
   processor: async (job: { data: TicketJobData }) => {
     const { ticketId, priority } = job.data;
 
-    console.log("📥 Ticket Job Received:", job.data);
+    logger.info({ data: job.data }, "📥 Ticket Job Received:");
 
     if (priority === TicketPriority.URGENT) {
-      console.log("📥 Urgent Ticket Job Received:", job.data);
+      logger.info(`📥 Urgent Ticket Job with ID ${ticketId} Received:`);
+
       // soon send email for admin here
+      const { data, error } = await resend.emails.send({
+        from: "Tickr <onboarding@resend.dev>",
+        to: "delivered@resend.dev",
+        subject: "Urgent Ticket Report",
+        html: "<strong>An urgent ticket report has been created</strong>",
+      });
+
+      if (error) throw Error(error.message);
+
+      logger.info({ data }, "📤 Urgent Ticket Job Sent");
     }
 
     return { status: "done" };
