cef70d77130dcc605a329f9fb3f20c951919cb86
ardianilyas <ardianilyas@gmail.com>
2025-12-07 14:03:12 +0700

    chore: add react email package

---
+++ react-email.config.js
@@ -0,0 +1,7 @@
+/** @type {import('react-email').Config} */
+module.exports = {
+    root: "./src/emails",
+    tailwind: {
+      config: "./tailwind.email.config.js",
+    },
+};  
---
+++ tailwind.email.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    content: ["./src/emails/**/*.{tsx,ts}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+};
