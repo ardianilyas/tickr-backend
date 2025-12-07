301dc4c2cc4b1b23b8b7a89c1693723f370d5df5
ardianilyas <ardianilyas@gmail.com>
2025-12-07 14:28:55 +0700

    feat: update ticket worker send email using react email

--- src/features/ticket/ticket.worker.ts
+++ src/features/ticket/ticket.worker.ts
@@ -1,3 +1,4 @@
+import TicketUrgentEmail from "../../emails/TicketUrgentEmail";
 import { TicketPriority } from "../../generated/prisma/enums";
 import { resend } from "../../lib/resend";
 import { logger } from "../../utils/logger";
@@ -8,19 +9,19 @@ export const ticketWorker = {
   queueName: TICKET_QUEUE_NAME,
 
   processor: async (job: { data: TicketJobData }) => {
-    const { ticketId, priority } = job.data;
+    const { priority, ticketCode } = job.data;
 
     logger.info({ data: job.data }, "📥 Ticket Job Received:");
 
     if (priority === TicketPriority.URGENT) {
-      logger.info(`📥 Urgent Ticket Job with ID ${ticketId} Received:`);
+      logger.info(`📥 Urgent Ticket Job with Ticket Code ${ticketCode} Received:`);
 
       // soon send email for admin here
       const { data, error } = await resend.emails.send({
         from: "Tickr <onboarding@resend.dev>",
         to: "delivered@resend.dev",
         subject: "Urgent Ticket Report",
-        html: "<strong>An urgent ticket report has been created</strong>",
+        react: TicketUrgentEmail({ username: "Ardian Ilyas", ticketCode }),
       });
 
       if (error) throw Error(error.message);
